572c1ad91904105a0604d1f8983fae138f291e05
Rebecca <ph.rdang@gmail.com>
2020-02-21 11:46:28 -0800

    Substitute magic numbers in servoProcess() with servo position constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearTeleop.java
@@ -16,12 +16,14 @@ public class LinearTeleop extends LinearOpMode {
     int armPos = 0;
     int count = 0;
 
-    public void runOpMode(){
+    public void runOpMode() {
         robot = new OmegaBot(telemetry,hardwareMap);//initializing hardware
         waitForStart();
         runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         robot.leftIntake.setPower(1);
         robot.rightIntake.setPower(-1);
+
+        // while running this opmode, continuously check gamepads for input
         while(opModeIsActive()){
             drivetrainProcess();
             armProcess();
@@ -30,12 +32,13 @@ public class LinearTeleop extends LinearOpMode {
             intakeProcessOut();
             foundationGrippers();
             //flagWave();
+
             telemetry.addData("armPos",robot.arm.getCurrentPosition());
             telemetry.update();
         }
     }
 
-    public void moveArm(double degrees){
+    public void moveArm(double degrees) {
         double maxVel = 15 * 360 / 60000; // 15 is the rotations per minute, 360 is the degrees per rotation, 60000 is the number of milliseconds in a minute
         double macAcc = maxVel / 1300; //1300 is the number of milliseconds it takes to accelerate to full speed
         MotionProfileGenerator generator = new MotionProfileGenerator(maxVel, macAcc);
@@ -47,43 +50,43 @@ public class LinearTeleop extends LinearOpMode {
         }
     }
 
-    public void flagWave(){
-        if(count < 50){
+    public void flagWave() {
+        if (count < 50) {
             robot.sideBackElbow.setPosition(0.3);
             count++;
-        }else if (count < 100){
+        } else if (count < 100) {
             robot.sideBackElbow.setPosition(0.35);
             count++;
-        }else{
+        } else {
             count = 0;
         }
     }
 
-    public void servoProcess(){
-        if(gamepad2.x){
-            robot.blockRotator.setPosition(0.62);
+    public void servoProcess() {
+        if (gamepad2.x) {
+            robot.blockRotator.setPosition(OmegaBot.BLOCK_ROTATOR_STRAIGHT);
+        } else if (gamepad2.y) {
+            robot.blockRotator.setPosition(OmegaBot.BLOCK_ROTATOR_ROTATED);
+        }
 
-        }else if(gamepad2.y){
-            robot.blockRotator.setPosition(.96);
+        if (gamepad2.right_bumper) {
+            robot.blockGripper.setPosition(OmegaBot.BLOCK_GRIPPER_OPEN);
+        } else if (gamepad2.left_bumper) {
+            robot.blockGripper.setPosition(OmegaBot.BLOCK_GRIPPER_CLOSED);
         }
-        if(gamepad2.right_bumper){
-            robot.blockGripper.setPosition(.5);
-        }else if(gamepad2.left_bumper){
-            robot.blockGripper.setPosition(.2);
-        }
-        if(gamepad1.x){
-            robot.sideBackGripper.setPosition(0);
-        }else if(gamepad1.y){
-            robot.sideBackGripper.setPosition(0.5);
+
+        if (gamepad1.x) {
+            robot.sideBackGripper.setPosition(OmegaBot.SIDE_BACK_GRIPPER_CLOSED);
+        } else if (gamepad1.y) {
+            robot.sideBackGripper.setPosition(OmegaBot.SIDE_BACK_GRIPPER_OPEN);
         }
     }
 
-    public void foundationGrippers(){
-        if(gamepad1.left_trigger > 0.5){
-            robot.foundationGripper.setPosition(1.00);
-        }
-        else if (gamepad1.right_trigger > 0.5){
-            robot.foundationGripper.setPosition(0.51);
+    public void foundationGrippers() {
+        if (gamepad1.left_trigger > 0.5) {
+            robot.foundationGripper.setPosition(OmegaBot.FOUNDATION_GRIPPER_DOWN);
+        } else if (gamepad1.right_trigger > 0.5) {
+            robot.foundationGripper.setPosition(OmegaBot.FOUNDATION_GRIPPER_UP);
         }
     }
 
@@ -99,10 +102,10 @@ public class LinearTeleop extends LinearOpMode {
             //moveArm(30);
             //robot.arm.setPower(-.75);
         } else if (gamepad2.dpad_down) {
-            robot.blockGripper.setPosition(0.75);
+            robot.blockGripper.setPosition(0.75); // what does this position refer to?
             armPos = -100;
         } else if (gamepad2.dpad_left) {
-            robot.blockGripper.setPosition(0.2);
+            robot.blockGripper.setPosition(0.2); // what does this position refer to?
             armPos = -210;
         } else if (gamepad2.dpad_up) {
             armPos = -900;
@@ -115,41 +118,35 @@ public class LinearTeleop extends LinearOpMode {
     }
 
     public void intakeProcessIn(){
-        if(gamepad2.right_trigger > .5) {
-            if (gamepad2.left_trigger > .5 && gamepad2.right_trigger > .5){
+        if (gamepad2.right_trigger > .5) {
+            if (gamepad2.left_trigger > .5 && gamepad2.right_trigger > .5) {
                 robot.leftIntake.setPower(0);
                 robot.rightIntake.setPower(0);
-            }
-            else {
+            } else {
                 robot.leftIntake.setPower(.3);
                 robot.rightIntake.setPower(-.3);
             }
 
-        }
-
-        else {
+        } else {
             robot.leftIntake.setPower(0);
             robot.rightIntake.setPower(0);
         }
     }
+
     public void intakeProcessOut(){
-        if (gamepad2.left_trigger > .5){
-            if (gamepad2.left_trigger > .5 && gamepad2.right_trigger > .5){
+        if (gamepad2.left_trigger > .5) {
+            if (gamepad2.left_trigger > .5 && gamepad2.right_trigger > .5) {
                 robot.leftIntake.setPower(0);
                 robot.rightIntake.setPower(0);
-            }
-            else {
+            } else {
                 robot.leftIntake.setPower(-1);
                 robot.rightIntake.setPower(1);
             }
 
-        }
-
-        else {
+        } else {
             robot.leftIntake.setPower(0);
             robot.rightIntake.setPower(0);
         }
-
     }
 
     public void drivetrainProcess(){
