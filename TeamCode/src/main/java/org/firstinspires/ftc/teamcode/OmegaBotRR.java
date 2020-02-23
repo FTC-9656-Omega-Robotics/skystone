5540a9208687dfd3acb99386cbc6f1493bb7a6ad
Rebecca <ph.rdang@gmail.com>
2020-02-23 11:00:21 -0800

    Updated arm constants to be consistent with OmegaBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmegaBotRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmegaBotRR.java
@@ -36,9 +36,9 @@ public class OmegaBotRR {
     public DcMotor rightIntake;
 
     // arm position constants
-    static final int ARM_INIT = -200;
+    static final int ARM_INIT = -250;
     static final int ARM_DOWN = -100;
-    static final int ARM_UP = -900;
+    static final int ARM_UP = -1400;
     static final int ARM_TRAVELING = -210;
 
     // servos
