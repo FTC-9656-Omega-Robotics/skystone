4146a31ac261186768bcc3974d5e2a523ec5beca
Rebecca <ph.rdang@gmail.com>
2020-02-23 11:00:02 -0800

    Added comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Blue3Stone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Blue3Stone.java
@@ -18,25 +18,30 @@ public class Blue3Stone extends LinearOpMode {
     OmegaBotRR robot;
     @Override
     public void runOpMode() throws InterruptedException {
+        // initialize robot and drivetrain
         robot = new OmegaBotRR(telemetry, hardwareMap);
         SampleMecanumDriveBase drive = new SampleMecanumDriveREV(hardwareMap);
 
         waitForStart();
 
         if (isStopRequested()) return;
+
+        // set initial position of the robot (x- and y-coordinates and heading)
         drive.setPoseEstimate(new Pose2d(-39,-63,0));
+
+        // commands inside the trajectory run one after another
         drive.followTrajectorySync(
                 drive.trajectoryBuilder()//new TrajectoryBuilder(new Pose2d(0,0,0), drive.getConstraints())
-                        .strafeTo(new Vector2d(-39,-39))
-                        .addMarker(() -> {
+                        .strafeTo(new Vector2d(-39,-39)) // strafe to coordinates (without turning)
+                        .addMarker(() -> { // addMarker to do things while following the trajectory
                             drive.setPoseEstimate(new Pose2d(-39,-27,0));
                             //add servos
                             return Unit.INSTANCE;
                         })
-                        .splineTo(new Pose2d(55,-35, 0))
-                        .reverse()
+                        .splineTo(new Pose2d(55,-35, 0)) // spline to coordinates (turns)
+                        .reverse() // makes robot go in reverse direction (rather than turning around to go to new Pose2d)
                         .splineTo(new Pose2d(-52,-39,0))
-                        .build()
+                        .build() // builds the path that I coded above
         );
 
 
