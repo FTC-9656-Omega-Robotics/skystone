2d98d1f52e5fed2919dd8b9dc0631da6fafa5cea
StealthX051 <stealthx051@gmail.com>
2020-02-24 17:18:35 -0800

    Not working code, laptop not working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmegaBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmegaBot.java
@@ -63,33 +63,33 @@ public class OmegaBot {
     public Servo capstone;
 
     // servo position constants
-    static final double BLOCK_ROTATOR_STRAIGHT = 0.62; // STRAIGHT is position for using blockGripper to intake
-    static final double BLOCK_ROTATOR_ROTATED = 0.96; // rotated 90 degrees from STRAIGHT
+    public static final double BLOCK_ROTATOR_STRAIGHT = 0.62; // STRAIGHT is position for using blockGripper to intake
+    public static final double BLOCK_ROTATOR_ROTATED = 0.96; // rotated 90 degrees from STRAIGHT
 
-    static final double BLOCK_GRIPPER_OPEN = 0.5;
-    static final double BLOCK_GRIPPER_CLOSED = 0.2;
+    public static final double BLOCK_GRIPPER_OPEN = 0.5;
+    public static final double BLOCK_GRIPPER_CLOSED = 0.2;
 
-    static final double FOUNDATION_GRIPPER_UP = 0.55;
-    static final double FOUNDATION_GRIPPER_DOWN = 1;
+    public static final double FOUNDATION_GRIPPER_UP = 0.55;
+    public static final double FOUNDATION_GRIPPER_DOWN = 1;
 
-    static final double SIDE_BACK_GRIPPER_OPEN = 0.41;
-    static final double SIDE_BACK_GRIPPER_CLOSED = 0.05;
-    static final double SIDE_BACK_GRIPPER_STOWED = 0.63; // stowed for robot inspection
+    public static final double SIDE_BACK_GRIPPER_OPEN = 0.41;
+    public static final double SIDE_BACK_GRIPPER_CLOSED = 0.05;
+    public static final double SIDE_BACK_GRIPPER_STOWED = 0.63; // stowed for robot inspection
 
-    static final double SIDE_BACK_ELBOW_UP = 0.31;
-    static final double SIDE_BACK_ELBOW_DOWN = 0;
+    public static final double SIDE_BACK_ELBOW_UP = 0.31;
+    public static final double SIDE_BACK_ELBOW_DOWN = 0;
 
-    static final double SIDE_FRONT_GRIPPER_OPEN = 0.91;
-    static final double SIDE_FRONT_GRIPPER_CLOSED = 0.56;
-    static final double SIDE_FRONT_GRIPPER_STOWED = 0.35; // stowed for robot inspection
+    public static final double SIDE_FRONT_GRIPPER_OPEN = 0.91;
+    public static final double SIDE_FRONT_GRIPPER_CLOSED = 0.56;
+    public static final double SIDE_FRONT_GRIPPER_STOWED = 0.35; // stowed for robot inspection
 
-    static final double SIDE_FRONT_ELBOW_UP = 0.33;
-    static final double SIDE_FRONT_ELBOW_DOWN = 0;
+    public static final double SIDE_FRONT_ELBOW_UP = 0.33;
+    public static final double SIDE_FRONT_ELBOW_DOWN = 0;
     //gripper sticks out, we will need a SIDE_FRONT_ELBOW_STOWED after phone is moved
-    // final double SIDE_FRONT_ELBOW_STOWED = [some value]; // stowed for robot inspection
+    // public final double SIDE_FRONT_ELBOW_STOWED = [some value]; // stowed for robot inspection
 
-    final double CAPSTONE_HELD = 0.9;
-    final double CAPSTONE_DROPPED = 0.28;
+    public final double CAPSTONE_HELD = 0.9;
+    public final double CAPSTONE_DROPPED = 0.28;
 
     // sensors
     public ColorSensor sensorColor;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Blue3Stone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Blue3Stone.java
@@ -17,6 +17,8 @@ import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREV;
 import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREVOptimized;
 
 
+import java.util.Vector;
+
 import kotlin.Unit;
 
 @Autonomous(group = "drive")
@@ -47,22 +49,42 @@ public class Blue3Stone extends LinearOpMode {
         phoneCam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_RIGHT);
 
 
+
+
+        //TODO: rename constants to all caps, and make skystone pos and dumping pos arrays
+        //TODO change constants to x and y ints instead of poses (bc strafe requires vectors)
+
+        // 1 is closest to bridge, 6 is closest to wall
+        // each stone is 8 in long
+        final Pose2d skystonePos1 = new Pose2d(-29, -39, 0);
+        final Pose2d skystonePos2 = new Pose2d(-37, -39, 0);
+        final Pose2d skystonePos3 = new Pose2d(-45, -39, 0);
+        final Pose2d skystonePos4 = new Pose2d(-53, -39, 0);
+        final Pose2d skystonePos5 = new Pose2d(-61, -39, 0);
+        final Pose2d skystonePos6 = new Pose2d(-69, -39, 0);
+
+        // a bit of space between robot and neutral bridge
+        final Pose2d underRedBridgePos = new Pose2d(0, -35, 0);
+
+        // far = close to wall, close = close to bridge
+        final Pose2d dumpingPosFar = new Pose2d(60, -35, 0);
+        final Pose2d dumpingPosMid = new Pose2d(55, -35, 0);
+        final Pose2d dumpingPosClose = new Pose2d(45, -35, 0);
+
+        // for testing: skystonePosWall is skystonePos4
+        int skystoneWallX = -53;
+        int skystoneWallY = -39;
+
+        // for testing: skystonePosBridge is skystonePos1
+        int skystoneBridgeX = -29;
+        int skystoneBridgeY = -39;
+
         // initialize these variables when the camera figures out
         // what the skystonePosition is (see while loop below)
-        Pose2d skystonePositionWall; // position of skystone closest to the wall
-        Pose2d skystonePositionBridge; // position of skystone closest to the bridge
+        Pose2d skystonePosWall = skystonePos4; // position of skystone closest to the wall
+        Pose2d skystonePosBridge = skystonePos1; // position of skystone closest to the bridge
 
-        /*
-        skystonePos for BLUE DEPOT SIDE, RED SKYBRIDGE
-        - each stone is 8 in long
-        1 (-29, -39)
-        2 (-37, -39)
-        3 (-45, -39)
-        4 (-53, -39)
-        5 (-61, -39)
-        6 (-69, -39)
-         */
-
+        Pose2d nearestStone = skystonePos2; // testing value only, same for 2 vars above
 
         double positionCorrector = 0;
         //All comments comment above what is being commented
@@ -88,7 +110,6 @@ public class Blue3Stone extends LinearOpMode {
             telemetry.update();
         }
         //TODO Find what numbers we need to set the positionCorrector to for each skystone position, then incorporate that into our spline code
-
 
         waitForStart();
 
@@ -143,15 +164,74 @@ public class Blue3Stone extends LinearOpMode {
         // commands inside the trajectory run one after another
         drive.followTrajectorySync(
                 drive.trajectoryBuilder()
-                        .strafeTo(new Vector2d(-39,-39)) // strafe to coordinates (without turning)
-                        .splineTo(new Pose2d(55,-35, 0)) // spline to coordinates (turns
-                        .reverse() // makes robot go in reverse direction (rather than turning around to go to new Pose2d)
-                        .splineTo(new Pose2d(-52,-39,0))//goes to pick up second block
-                        .reverse()//reverses the reverse so it's normal again
-                         .splineTo(new Pose2d(49,-35,0))//goes to drop of second block
+                        .strafeTo(new Vector2d(skystoneWallX, skystoneWallY)) // strafe to first skystone (closest to wall)
+                        .addMarker( () -> {
+                            // pick up first skystone
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_DOWN);
+                            sleep(500);
+                            robot.sideBackGripper.setPosition(OmegaBotRR.SIDE_BACK_GRIPPER_CLOSED);
+                            sleep(500);
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_UP);
+                            sleep(500);
+
+                            return Unit.INSTANCE;
+                        })
+                        .splineTo(underRedBridgePos) // spline to under red bridge
+                        .splineTo(dumpingPosFar)// spline to foundation far dumping position
+                        .addMarker( () -> {
+                            // dump skystone
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_DOWN);
+                            sleep(500);
+                            robot.sideBackGripper.setPosition(OmegaBotRR.SIDE_BACK_GRIPPER_OPEN);
+                            sleep(500);
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_UP);
+                            sleep(500);
+
+                            return Unit.INSTANCE;
+                        })
+                        .reverse()// reverse direction to go back for stone
+                        .splineTo(underRedBridgePos) // spline back under bridge
+                        .addMarker( () -> {
+                            // put side front elbow in ready position
+                            robot.sideBackElbow.setPosition(0.15); // 0.15 is testing for ready pos, TODO make constant
+                            sleep(500);
+
+                            return Unit.INSTANCE;
+                        })
+                        .splineTo(skystonePosBridge) // spline to second skystone (closest to bridge)
+                        .addMarker( () -> {
+                            // pick up second skystone
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_DOWN);
+                            sleep(500);
+                            robot.sideBackGripper.setPosition(OmegaBotRR.SIDE_BACK_GRIPPER_CLOSED);
+                            sleep(500);
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_UP);
+                            sleep(500);
+
+                            return Unit.INSTANCE;
+                        })
+                        .reverse() //reverses the reverse so it goes forward again
+                        .splineTo(underRedBridgePos)// spline to under bridge
+                        .splineTo(dumpingPosMid) // splines to foundation to dump skystone
+                        .addMarker( () -> {
+                            // dump second skystone
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_DOWN);
+                            sleep(500);
+                            robot.sideBackGripper.setPosition(OmegaBotRR.SIDE_BACK_GRIPPER_OPEN);
+                            sleep(500);
+                            robot.sideBackElbow.setPosition(OmegaBotRR.SIDE_BACK_ELBOW_UP);
+                            sleep(500);
+
+                            return Unit.INSTANCE;
+                        })
+                        .reverse() // reverse direction to go back for stone
+                        // do third (regular) stone
                         .build()
 
         );
+
+        //TODO split up trajectory above into smaller ones to avoid emergency stop error
+        /*
 
         drive.followTrajectorySync(
                 drive.trajectoryBuilder()
@@ -161,5 +241,6 @@ public class Blue3Stone extends LinearOpMode {
                         .splineTo(new Pose2d(40,-35,0))//goes to drop off 3d block
                         .build()
         );
+        */
     }
 }
